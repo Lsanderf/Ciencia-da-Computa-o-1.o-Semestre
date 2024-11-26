47300f11aa964107333c0588d7ed8e6ef3293d81
LUCCA <lsanderf08@gmail.com>
2024-11-26 18:59:41 -0300

    Add

---
+++ Aeds-1/Conteúdos/Archive/Add_Archive.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){ 
+    FILE *arq = fopen("C:\\Users\\Lucca\\Desktop\\Arquivo\\Arquivo Teste", "a");
+    if(arq != NULL){ 
+    fprintf(arq, "\nAdd mensages");
+    }
+    fclose(arq);
+}
